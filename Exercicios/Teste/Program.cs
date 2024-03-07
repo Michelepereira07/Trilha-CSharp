abe9c70f89601c89a4a5daf0d34606139acdb5ae
Michelepereira07 <105759846+Michelepereira07@users.noreply.github.com>
2024-03-07 15:01:43 -0300

    Exercicio iniciante

---
+++ Exercicios/Teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double n = 3.14159;
+        double raio = 2.00;
+
+        double area = n * (raio * 2);
+
+        Console.WriteLine("A= " + area);
+
+    }
+
+}

f943f4a2d24304b83f89662c7af61609de4779b4
Michelepereira07 <105759846+Michelepereira07@users.noreply.github.com>
2024-03-13 11:25:43 -0300

    Salario

---
+++ Exercicios/Salario/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using System.Globalization;
+
+int funcionario= 1;
+int horas= 200;
+double salario = 20.50;
+
+var resultado = horas * salario;
+
+var teste = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "US$ {0:####.##}", resultado);
+
+Console.WriteLine($"NUMBER = {funcionario}");
+Console.WriteLine($"SALARY = {resultado}");
+Console.WriteLine(teste);

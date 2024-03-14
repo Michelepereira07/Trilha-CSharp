9539a53ba9cdeba44a631b7eef04bff72bae48fb
Michelepereira07 <105759846+Michelepereira07@users.noreply.github.com>
2024-03-14 14:02:40 -0300

    Salário com Bônus

---
+++ Exercicios/SalarioComBonus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+string name = Console.ReadLine();
+//converter
+double salario = double.Parse(Console.ReadLine(),CultureInfo.CreateSpecificCulture("en-US"));
+double totalVendas = double.Parse(Console.ReadLine(), CultureInfo.CreateSpecificCulture("en-US"));
+
+
+double salarioLiquido = salario + (totalVendas * 0.15);
+
+Console.WriteLine("TOTAL = R$ " + salarioLiquido.ToString("F2", CultureInfo.CreateSpecificCulture("EN-us")));
+Console.ReadLine();
+
+
+
+
+
+
+

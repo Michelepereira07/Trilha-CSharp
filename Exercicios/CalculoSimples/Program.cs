27cb705234a6a3a0be48d07bc92559b3b4d5c00c
Michelepereira07 <105759846+Michelepereira07@users.noreply.github.com>
2024-03-19 10:54:25 -0300

    Cálculo Simples

---
+++ Exercicios/CalculoSimples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+int  codigo1, peca1;
+int  codigo2, peca2;
+double valor1, valor2, valor;
+
+codigo1 = int.Parse(Console.ReadLine());
+peca1 = int.Parse(Console.ReadLine());
+valor1 = double.Parse(Console.ReadLine(), CultureInfo.CreateSpecificCulture("pt-BR"));
+codigo2 = int.Parse(Console.ReadLine());
+peca2 = int.Parse(Console.ReadLine());
+valor2 = double.Parse(Console.ReadLine(), CultureInfo.CreateSpecificCulture("pt-BR"));
+
+
+valor = peca1 * valor1 + peca2 * valor2;
+
+Console.WriteLine("VALOR A PAGAR: R$ " + valor.ToString("F2", CultureInfo.CreateSpecificCulture("pt-BR")));
+Console.ReadLine();

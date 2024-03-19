355b0d1467a94af5f36c40a3bc64e64b7fa70423
Michelepereira07 <105759846+Michelepereira07@users.noreply.github.com>
2024-03-19 13:21:58 -0300

    Calculo do volume de uma esfera.

---
+++ Exercicios/Esfera/Program.cs
@@ -0,0 +1,12 @@
+﻿int raio;
+double pi;
+double volume;
+
+pi = 3.14159;
+raio = int.Parse(Console.ReadLine());
+
+volume = (4 / 3.0) * pi * Math.Pow(raio, 3);
+
+Console.WriteLine("VOLUME = " + volume);
+
+

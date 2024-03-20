bfbf91148c6e1a49fc06c4cc1a27fd6b5f23ea83
Michelepereira07 <105759846+Michelepereira07@users.noreply.github.com>
2024-03-20 15:06:31 -0300

    Programa para ler três valores e apresentar o maior dos três.

---
+++ Exercicios/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+string[] Area = Console.ReadLine().Split(' ');
+
+double A = double.Parse(Area[0], CultureInfo.InvariantCulture);
+double B = double.Parse(Area[1], CultureInfo.InvariantCulture);
+double C = double.Parse(Area[2], CultureInfo.InvariantCulture);
+
+Console.WriteLine($"TRIANGULO: {(A * C / 2).ToString("F3", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"CIRCULO: {(A * C / 2).ToString("F3", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"TRAPEZIO: {(A * C / 2).ToString("F3", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"QUADRADO: {(A * C / 2).ToString("F3", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"RETANGULO: {(A * C / 2).ToString("F3", CultureInfo.InvariantCulture)}");
+
+Console.ReadKey();
---
+++ Exercicios/MaiordeTresValores/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] Maior = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(Maior[0]);
+        int b = int.Parse(Maior[1]);
+        int c = int.Parse(Maior[2]);
+
+        int MaiorAB = (a + b + Math.Abs(a - b)) / 2;
+        int Omaior = (MaiorAB + c + Math.Abs(MaiorAB - c)) / 2;
+
+        Console.WriteLine($"{Omaior} + eh o maior ");
+        Console.ReadKey();
+    }
+}

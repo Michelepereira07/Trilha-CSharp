e7f9f583299e9fbe5c535d123500d8012758d4c1
Michelepereira07 <105759846+Michelepereira07@users.noreply.github.com>
2024-03-21 14:26:26 -0300

    Consumo médio de um automóvel.

---
+++ Exercicios/ConsumoCarro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        decimal y = Convert.ToDecimal(Console.ReadLine());
+        var consumo = x / y;
+        
+        Console.WriteLine($"{consumo.ToString("F3", CultureInfo.InvariantCulture)} + Lm/L");
+
+        Console.ReadLine();
+    }
+}
